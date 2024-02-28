9f5d81a9f21e5bccf2e6c606a989da09e156036b
RuchikaKadam <kadamruchikafullstack@gmail.com>
2024-02-28 14:27:39 +0530

    day 2 classwork

---
+++ day2/day2.js
@@ -0,0 +1,4 @@
+const reactFunction() { //this is a functional component
+    return <div>This is first Div in the first Function</div>;
+}
+ReactDOM.render(<reactFunction />, document.querySelector("#root"));
