63b0b840ec903197720ff0c24ddfd283edf9c7a9
RuchikaKadam <kadamruchikafullstack@gmail.com>
2024-02-29 12:53:59 +0530

    day 3 classwork

--- day2/day2.js
+++ day2/day2.js
@@ -1,4 +1,15 @@
-const reactFunction() { //this is a functional component
-    return <div>This is first Div in the first Function</div>;
-}
-ReactDOM.render(<reactFunction />, document.querySelector("#root"));
+// const reactFunction() { //this is a functional component
+//     return <div>This is first Div in the first Function</div>;
+// }
+// ReactDOM.render(<reactFunction />, document.querySelector("#root"));
+
+
+const h1 = React.createElement('h2',{style:{
+    color: 'grey'},
+    id:"h1inRoot"
+}, 
+"Learn React with MDN!")
+
+const p = React.createElement('p',{}, "HI! I am a paragraph inside a div and I am a sibling element of H1.")
+const div = React.createElement('div',{}, [h1, p])
+ReactDOM.render(div, document.getElementById('root'))
