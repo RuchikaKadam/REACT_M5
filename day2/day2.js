151809bb3730015223d3e00b0b121e43507911b3
RuchikaKadam <kadamruchikafullstack@gmail.com>
2024-03-05 12:30:23 +0530

    day 2 homework added

--- day2/day2.js
+++ day2/day2.js
@@ -3,13 +3,32 @@
 // }
 // ReactDOM.render(<reactFunction />, document.querySelector("#root"));
 
-
+//day 2 homework
 const h1 = React.createElement('h2',{style:{
-    color: 'grey'},
-    id:"h1inRoot"
+    color: 'white'},
+    id:"h1inRoot",
+    className: "heading"
 }, 
-"Learn React with MDN!")
+"REACT  : Day 2 homework")
 
-const p = React.createElement('p',{}, "HI! I am a paragraph inside a div and I am a sibling element of H1.")
-const div = React.createElement('div',{}, [h1, p])
-ReactDOM.render(div, document.getElementById('root'))
+const heading = React.createElement('h2',{}, "Topics covered")
+const des = React.createElement('p',{}, "The following is a list of all the topics we cover in the MDN learning area.")
+const p1 = React.createElement('p',{}, "Provides a practical introduction to web development for complete beginners.")
+const p2 = React.createElement('p',{}, "HTML is the language that we use to structure the different parts of our content and define what their meaning or purpose is. This topic teaches HTML in detail.")
+const p3 = React.createElement('p',{}, "CSS is the language that we use to control our web content's style and layout, as well as adding behavior like animation. This topic provides comprehensive coverage of CSS.")
+const a1 = React.createElement('a',{}, "Getting started with the web")
+const a2 = React.createElement('a',{}, "HTML- Structuring the web")
+const a3 = React.createElement('a',{}, "CSS-Styling the web")
+
+
+const hwDiv = React.createElement('div',{}, [h1, heading, des, a1, p1, a2, p2, a3, p3]);
+ReactDOM.render(hwDiv, document.getElementById('root'));
+
+
+
+//day 2 classwork
+
+
+
+
+
