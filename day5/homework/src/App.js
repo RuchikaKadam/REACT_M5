c14f71d6d771084d442711dc1a95f3e224fdd009
RuchikaKadam <kadamruchikafullstack@gmail.com>
2024-03-10 23:50:21 +0530

    day 5 homework completed

---
+++ day5/homework/src/App.js
@@ -0,0 +1,35 @@
+import './App.css';
+import React, {useState} from 'react';
+import {data} from './constant';
+
+function App() {
+  const [count, setCount] = useState(0);
+  const [text, setText] = useState([]);
+  
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    let amount = parseInt(count);
+    if(amount < 0) amount = 1;
+    if(amount > 20) amount = 20;
+    setText(data.slice(0,amount));
+    return;
+  };
+  return (
+    <div className="App">
+      <section className='section-center'>
+          <h3>Alternative for lorem ipsum!</h3>
+          <form className='lorem-form' onSubmit={handleSubmit}>
+            <input type="number" name="amount" id="amount" placeholder='Number of Paragraphs' value={count} onChange={(e)=>setCount(e.target.value)}/>
+            <button className='btn'>Generate Paragraphs</button>
+          </form>
+          <article>
+            {text.map((data,index) => (
+              <p key={index}>{data}</p>
+            ))}
+          </article>
+      </section>
+    </div>
+  );
+}
+
+export default App;
